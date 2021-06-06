9a78c1e2bb7a58fb30b4e260d7d3913ec69cd9f7
kytosai <kytosai@gmail.com>
2021-06-06 21:45:06 +0700

    change package name

--- android/app/src/main/java/com/fadowallet/MainActivity.java
+++ android/app/src/main/java/com/fadowallet/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kardiawallet;
+package com.fadowallet;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "KardiaChainWallet";
+    return "FadoWallet";
   }
 }
--- ios/FadoWalletTests/FadoWalletTests.m
+++ ios/FadoWalletTests/FadoWalletTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KardiaChainWalletTests : XCTestCase
+@interface FadoWalletTests : XCTestCase
 
 @end
 
-@implementation KardiaChainWalletTests
+@implementation FadoWalletTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
